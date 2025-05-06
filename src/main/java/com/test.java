d936111741871bdb879673f6ddb78c3f9fe60382
Pocholo Abon <166315810+Coderoamo@users.noreply.github.com>
2025-05-06 06:57:20 +0000

    ples

--- src/main/java/com/test.java
+++ src/main/java/com/test.java
@@ -2,6 +2,7 @@ package com;
 public class test {
     public static void main(String[] args) {
         System.out.println("HelloWolrd!");
+        System.out.println("HelloWolrd!");
     }
     
 }
