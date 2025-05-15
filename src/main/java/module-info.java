b01ff641749d0f50075f3933ce221c0d315bd5db
Pocholo Abon <166315810+Coderoamo@users.noreply.github.com>
2025-05-15 05:56:22 +0000

    commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.leave.engine {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens com.leave.engine to javafx.fxml;
     exports com.leave.engine;
